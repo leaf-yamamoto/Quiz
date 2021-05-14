a9660be34f893eb3c2956242ada4276b17c41844
leaf-yamamoto <sanaeyamamoto@icloud.com>
2021-05-15 01:33:39 +0900

    クイズアプリ（起動画面）完成

---
+++ app/src/main/java/app/yamamoto/leaf/quiz/QuizActivity.kt
@@ -0,0 +1,16 @@
+package app.yamamoto.leaf.quiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+
+
+
+    }
+}
